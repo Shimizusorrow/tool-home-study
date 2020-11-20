911b2cbffe8b878cf40400a27d5d160ae8d0b742
shimizu <shimizusorrow>
2020-11-20 13:54:17 +0800

    对数组元素 进行整合

--- hutool/src/test/java/com/shimizu/hutool/StreamTest.java
+++ hutool/src/test/java/com/shimizu/hutool/StreamTest.java
@@ -7,14 +7,42 @@ import org.mockito.internal.matchers.ArrayEquals;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @ExtendWith(SpringExtension.class)
 public class StreamTest {
+
+    /**
+     * 对数组元素 进行整合
+     */
+    @Test
+    void testGroupBy() {
+        ArrayList<User> users = new ArrayList<>(Arrays.asList(
+                new User("张三", "男", 18),
+                new User("李四", "男", 18),
+                new User("王五", "男", 18),
+                new User("张三", "女", 16),
+                new User("张三", "女", 15),
+                new User("赵六", "男", 15),
+                new User("江七", "男", 20)
+        ));
+
+        Map<String, User> map = new HashMap<>();
+        users.stream().forEach(it -> {
+            if (!map.containsKey(it.name + it.gender)) {
+                map.put(it.name + it.gender, it);
+            } else {
+                User user = map.get(it.name + it.gender);
+                user.age = user.age + it.age;
+                map.put(it.name + it.gender, user);
+            }
+        });
+        List<User> users1 = map.entrySet().stream().map(it -> it.getValue()).collect(Collectors.toList());
+        users1.forEach(it -> {
+            System.out.println(it.name + it.gender + ":" + it.age);
+        });
+    }
 
     /**
      * collectingAndThen 方法对数组对象or元素去重
@@ -60,6 +88,7 @@ public class StreamTest {
             this.gender = gender;
             this.age = age;
         }
+
         @Override
         public String toString() {
             return "User{" +
