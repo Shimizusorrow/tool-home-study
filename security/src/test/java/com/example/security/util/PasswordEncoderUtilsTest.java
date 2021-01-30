80eba9fae1bcb2bcc4d3424621fca6fb58d47fa7
shimizu <shimizusorrow>
2021-01-30 13:02:13 +0800

    测试匹配

--- security/src/test/java/com/example/security/util/PasswordEncoderUtilsTest.java
+++ security/src/test/java/com/example/security/util/PasswordEncoderUtilsTest.java
@@ -1,13 +1,23 @@
 package com.example.security.util;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordEncoderUtilsTest {
     @Test
-    void decode() {
+    void encode() {
         System.out.println(PasswordEncoderUtils
                 .getBCryptPasswordEncoder().encode("4"));
+    }
+
+    @Test
+    void decode() {
+        PasswordEncoder encoder = PasswordEncoderUtils.getBCryptPasswordEncoder();
+        String encode = encoder.encode("5");
+        System.out.println(encode);
+        System.out.println(encoder.matches("5", encode));
+
     }
 }
