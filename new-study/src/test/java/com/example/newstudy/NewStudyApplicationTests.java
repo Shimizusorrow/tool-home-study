66d2030613995c0111c37a104576d3e1f32fee51
shimizu <shimizusorrow>
2021-05-27 14:23:11 +0800

    学习测试

---
+++ new-study/src/test/java/com/example/newstudy/NewStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ new-study/src/test/java/com/example/newstudy/clone/CloneTest.java
@@ -0,0 +1,9 @@
+package com.example.newstudy.clone;
+
+/**
+ *
+ * @author Shimizu
+ * @date 2021-05-27 14:22
+ */
+public class CloneTest {
+}
